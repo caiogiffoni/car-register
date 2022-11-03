1be294b4b81f120c3a7c9cea14e472c141c8076b
Caio Giffoni <caio_cgf@hotmail.com>
2022-11-03 20:39:09 -0300

    :bug: fix: cant create car already created

--- server/src/services/car/createCar.service.ts
+++ server/src/services/car/createCar.service.ts
@@ -1,5 +1,6 @@
 import { AppDataSource } from "../../data-source";
 import { Car } from "../../entities/Car";
+import { AppError } from "../../errors/appError";
 import { ICarCreate, ICar } from "../../interfaces/car";
 
 export const createCarService = async ({
@@ -10,6 +11,22 @@ export const createCarService = async ({
   shift,
 }: ICarCreate): Promise<ICar> => {
   const carRepository = AppDataSource.getRepository(Car);
+
+  // Procurar por carro já existente
+  const findCar = await carRepository.findOne({
+    where: {
+      brand,
+      model,
+      year_fabrication,
+      year_model,
+      shift,
+    },
+  });
+
+  // Retornar erro se carro ja existente estiver no banco
+  if (findCar) {
+    throw new AppError(403, "This car already exists");
+  }
 
   const car = carRepository.create({
     brand,
