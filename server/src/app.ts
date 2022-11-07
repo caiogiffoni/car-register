315c59152effdf2fe3c3be0f9e09c2ea4fc0d715
Caio Giffoni <caio_cgf@hotmail.com>
2022-11-07 13:15:25 -0300

    :sparkles: added cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,12 +3,17 @@ import "express-async-errors";
 import { appRoutes } from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
 
+let cors = require("cors");
+
 const app = express();
 
 app.use(express.json());
 
+app.use(cors());
+
 appRoutes(app);
 
 app.use(errorMiddleware);
+
 
 export default app;
