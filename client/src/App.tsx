af63249babb82ad21132ae1c00904590b258023f
Caio Giffoni <caio_cgf@hotmail.com>
2022-11-07 13:16:59 -0300

    :sparkles: added form and send request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useForm } from "react-hook-form";
+
+import "./App.css";
+import { Box, Heading, Text } from "@chakra-ui/layout";
+import { Input } from "@chakra-ui/input";
+import { Button } from "@chakra-ui/button";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { InputForm } from "./components/InputForm/Input";
+import api from "./services";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const schema = yup.object().shape({
+    brand: yup.string().required("Campo obrigatório!"),
+    model: yup.string().required("Campo obrigatório!"),
+    color: yup.string().required("Campo obrigatório!"),
+    year_fabrication: yup
+      .number()
+      .typeError("Ano deve ser um número")
+      .required("Campo obrigatório!"),
+    year_model: yup
+      .number()
+      .typeError("Ano deve ser um número")
+      .required("Campo obrigatório!"),
+    shift: yup.string().required("Campo obrigatório!"),
+  });
+
+  interface IPost {
+    brand: string;
+    model: string;
+    color: string;
+    year_fabrication: number;
+    year_model: number;
+    shift: string;
+  }
+
+  const onSubmitFunction = async (data: IPost) => {
+    console.log("envieou");
+    api
+      .post("/car", data)
+      .then((res) => {
+        console.log("ok");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IPost>({
+    resolver: yupResolver(schema),
+  });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Box bgColor="#c9c4c4" p="15px" borderRadius="15px">
+      <Heading>Cadastro de Carros</Heading>
+      <Box
+        w="600px"
+        minH="320px"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-between"
+        gap="8px"
+        mt="15px"
+      >
+        <InputForm
+          placeholder="Insira a marca do carro"
+          error={errors.brand}
+          colorWordsDesc="black"
+          {...register("brand")}
+        />
+        <InputForm
+          placeholder="Insira a modelo do carro"
+          error={errors.model}
+          colorWordsDesc="black"
+          {...register("model")}
+        />
+        <InputForm
+          placeholder="Insira a cor do carro"
+          error={errors.color}
+          colorWordsDesc="black"
+          {...register("color")}
+        />
+        <InputForm
+          placeholder="Insira o ano de fabricação do carro"
+          error={errors.year_fabrication}
+          colorWordsDesc="black"
+          {...register("year_fabrication")}
+        />
+        <InputForm
+          placeholder="Insira o ano do modelo carro"
+          error={errors.year_model}
+          colorWordsDesc="black"
+          {...register("year_model")}
+        />
+        <InputForm
+          placeholder="Insira o tipo de câmbio do carro"
+          error={errors.shift}
+          colorWordsDesc="black"
+          {...register("shift")}
+        />
+      </Box>
+      <Button
+        bgColor="#365fe6"
+        mt="15px"
+        color="white"
+        onClick={handleSubmit(onSubmitFunction)}
+      >
+        Enviar
+      </Button>
+    </Box>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/InputForm/Input.tsx
@@ -0,0 +1,44 @@
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input as ChakraInput,
+  InputGroup,
+  InputProps as ChakraInputProps,
+} from "@chakra-ui/react";
+import { ForwardRefRenderFunction, forwardRef } from "react";
+import { FieldError } from "react-hook-form";
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+  error?: FieldError | null;
+  colorWordsDesc: string;
+}
+
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, error = null, label, colorWordsDesc, ...rest },
+  ref
+) => {
+  return (
+    <FormControl isInvalid={!!error}>
+      {label && <FormLabel color={colorWordsDesc}>{label}</FormLabel>}
+      <InputGroup flexDirection={"column"}>
+        <ChakraInput
+          name={name}
+          bgColor="gray.50"
+          variant="outline"
+          _hover={{ bgColor: "gray.10" }}
+          _placeholder={{ color: "gray.500" }}
+          ref={ref}
+          {...rest}
+        />
+        {!!error && (
+          <FormErrorMessage color="red.500">{error.message}</FormErrorMessage>
+        )}
+      </InputGroup>
+    </FormControl>
+  );
+};
+
+export const InputForm = forwardRef(InputBase);
