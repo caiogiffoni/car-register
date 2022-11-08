5b77e1e88874cec5c10eb860bdf97f1550ca6fad
Caio Giffoni <caio_cgf@hotmail.com>
2022-11-08 18:16:50 -0300

    :sparkles: added back end deploy to services

--- client/src/services/index.tsx
+++ client/src/services/index.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://car-register-caio-giffoni.herokuapp.com/",
 });
+// http://localhost:3000/
 
 export default api;
